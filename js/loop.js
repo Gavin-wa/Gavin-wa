df8c4b65393a69f9efb4cf012ac68dc6301543f0
Gavin-wa <74559646+Gavin-wa@users.noreply.github.com>
2021-01-19 15:58:39 -0500

    2.4.1
    Made the webpage deli-friendly

--- js/loop.js
+++ js/loop.js
@@ -3,6 +3,7 @@ var operator = 'addition'; // Type of calculation
 var i = 1; // Set counter to 1
 var msg = ''; // Message
 var rng = Math.floor((Math.random() * 10) + 1);
+var dayNames = ['Filler', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 if (rng % 2 === 0) {
 	var operator = 'notaddition';
@@ -10,22 +11,22 @@ if (rng % 2 === 0) {
 	var operator = 'addition';
 }
 
-
 if (operator === 'addition') {
 	// Do addition
-	while (i < 11) {
-		msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+	while (i < 8) {
+		msg += dayNames[i] + ' Prices: ' + '$' + (i+table) + '<br />';
 		i++;
 		table++;
 	}
 } else {
 	// Do multiplication
-	while (i < 11) {
-		msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+	while (i < 8) {
+		msg += dayNames[i] + ' Prices: ' + '$' + (i*table) + '<br />';
 		i++;
 		table++;
 	}
 }
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
